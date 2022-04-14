a5507cd914c94b6fd4accf1a050a04009ea1598a
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-04-14 20:19:27 +0100

    setting up RQ for data fetch and RQDevtools

--- src/App.js
+++ src/App.js
@@ -3,34 +3,40 @@ import "./App.css";
 import { HomePage } from "./components/Home.page";
 import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
 import { SuperHeroesPage } from "./components/SuperHeroes.page";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/super-heroes" element={<SuperHeroesPage />}></Route>
-          <Route
-            path="/rq-super-heroes"
-            element={<RQSuperHeroesPage />}
-          ></Route>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/super-heroes" element={<SuperHeroesPage />}></Route>
+            <Route
+              path="/rq-super-heroes"
+              element={<RQSuperHeroesPage />}
+            ></Route>
+            <Route path="/" element={<HomePage />}></Route>
+          </Routes>
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
 }
 
