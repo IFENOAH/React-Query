0067ddd7472cd9735ba3b08e353d8ddd42f33900
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-04-15 12:57:48 +0100

    creating custom query fetch hook

---
+++ src/Hooks/useSuperHeroData.js
@@ -0,0 +1,16 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+export const useSuperHeroData = (onSuccess, onError) => {
+  return useQuery("super-heroes", fetchSuperHeroes, {
+    onSuccess,
+    onError,
+    select: (data) => {
+      const superHeroNames = data.data.map((hero) => hero.name);
+      return superHeroNames;
+    },
+  });
+};
--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,10 +1,6 @@
-import axios from "axios";
 import React from "react";
-import { useQuery } from "react-query";
+import { useSuperHeroData } from "../Hooks/useSuperHeroData";
 
-const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
-};
 export const RQSuperHeroesPage = () => {
   const onSuccess = (data) => {
     console.log("Side Effect after success", data);
@@ -12,18 +8,8 @@ export const RQSuperHeroesPage = () => {
   const onError = (error) => {
     console.log("side Effect after Error", error);
   };
-  const { isLoading, data, isError, isFetching, error, refetch } = useQuery(
-    "super-heroes",
-    fetchSuperHeroes,
-    {
-      onSuccess,
-      onError,
-      select: (data) => {
-        const superHeroNames = data.data.map((hero) => hero.name);
-        return superHeroNames;
-      },
-    }
-  );
+  const { isLoading, data, isError, isFetching, error, refetch } =
+    useSuperHeroData(onSuccess, onError);
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
