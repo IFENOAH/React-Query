b4c32e13c480024267708f87f10b022a3e2b060f
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-04-18 15:50:56 +0100

    optimistic updates for better performance

--- src/Hooks/useSuperHeroData.js
+++ src/Hooks/useSuperHeroData.js
@@ -20,7 +20,35 @@ export const useSuperHeroData = (onSuccess, onError) => {
 export const useAddHeroData = () => {
   const queryClient = useQueryClient();
   return useMutation(addHero, {
-    onSuccess: () => {
+    // onSuccess: (data) => {
+    //   // queryClient.invalidateQueries("super-heroes");
+    //   queryClient.setQueryData("super-heroes", (oldQueryData) => {
+    //     return {
+    //       ...oldQueryData,
+    //       data: [...oldQueryData.data, data.data],
+    //     };
+    //   });
+    // },
+    onMutate: async (newHero) => {
+      await queryClient.cancelQueries("super-heroes");
+      const previousHeroData = queryClient.getQueryData("super-heroes");
+      queryClient.setQueryData("super-heroes", (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
+        };
+      });
+      return {
+        previousHeroData,
+      };
+    },
+    onError: (_error, _hero, context) => {
+      queryClient.setQueryData("super-heroes", context.previousHeroData);
+    },
+    onSettled: () => {
       queryClient.invalidateQueries("super-heroes");
     },
   });
